af249b1052f250059356e5a89c080561b38d6064
yujung7768903 <yu-jung31476@naver.com>
2022-07-04 23:35:24 +0900

    Create spring project

---
+++ src/main/java/com/godofparking/jeogidabackend/JeogidaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.godofparking.jeogidabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeogidaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeogidaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godofparking/jeogidabackend/JeogidaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godofparking.jeogidabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeogidaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
