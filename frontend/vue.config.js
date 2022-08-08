bc383c2e82682663546588fc316dd82c05eecc9e
yujung7768903 <yu-jung31476@naver.com>
2022-08-08 15:34:02 +0900

    cors 에러 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
             // 프록시 요청을 보낼 api의 시작 부분
             '/parkinglot': {
                 // 프록시 요청을 보낼 서버의 주소
-                target: 'http://ec2-3-37-217-255.ap-northeast-2.compute.amazonaws.com:8080/',
+                target: 'http://ec2-3-37-217-255.ap-northeast-2.compute.amazonaws.com:8080',
                 changeOrigin: true // cross origin 허용
             }
         }
