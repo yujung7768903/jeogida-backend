2e194130a16342ae570206bd5c89eb30ac4069e0
yujung7768903 <yu-jung31476@naver.com>
2022-08-21 03:25:33 +0900

    인지 검증과 예외 처리 추가

--- src/main/java/com/godofparking/jeogidabackend/dto/UserSaveRequestDto.java
+++ src/main/java/com/godofparking/jeogidabackend/dto/UserSaveRequestDto.java
@@ -1,2 +1,38 @@
-package com.godofparking.jeogidabackend.dto;public class UserSaveRequestDto {
+package com.godofparking.jeogidabackend.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class UserSaveRequestDto {
+
+    @NotBlank(message = "이메일은 빈 값을 가질 수 없습니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @ApiModelProperty(example = "yj06283l4@gmail.com")
+    private String email;
+
+    @NotBlank(message = "이름은 빈 값을 가질 수 없습니다.")
+    @ApiModelProperty(example = "김유정")
+    private String nickname;
+
+    @ApiModelProperty(example = "https://lh3.googleusercontent.com/a-/AFdZucpR_TN-i3qs-t0dQk-LFdHqSIUq_Wvv10iwcAIKSw=s96-c")
+    private String photo_url;
+
+    @NotBlank(message = "코드는 빈 값을 가질 수 없습니다.")
+    @ApiModelProperty(example = "104480857081468893438")
+    private String code;
+
+    public UserDto toUser() {
+        return UserDto.builder()
+                .email(email)
+                .nickname(nickname)
+                .role(Role.USER)
+                .photo_url(photo_url)
+                .code(code)
+                .build();
+    }
+
 }
