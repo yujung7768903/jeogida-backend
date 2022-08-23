729df0ea73576eb031abb840f7a37329c90bd425
yujung7768903 <yu-jung31476@naver.com>
2022-08-23 16:17:21 +0900

    save 요청을 위해 ParkingLotSaveRequestDto 추가

--- src/main/java/com/godofparking/jeogidabackend/dto/ParkingLotSaveRequestDto.java
+++ src/main/java/com/godofparking/jeogidabackend/dto/ParkingLotSaveRequestDto.java
@@ -1,2 +1,35 @@
-package com.godofparking.jeogidabackend.dto;public class ParkingLotSaveRequestDto {
+package com.godofparking.jeogidabackend.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ParkingLotSaveRequestDto {
+
+    @NotBlank(message = "층 수 이름은 빈 값을 가질 수 없습니다.")
+    @ApiModelProperty(example = "B1")
+    private String name;
+
+    @NotNull(message = "장소 아이디는 빈 값을 가질 수 없습니다.")
+    @Min(value = 1, message = "아이디는 0 또는 음수 값을 가질 수 없습니다.")
+    @ApiModelProperty(example = "1")
+    private Integer location_id;
+
+    @NotNull(message = "지도 아이디는 빈 값을 가질 수 없습니다.")
+    @Min(value = 1, message = "아이디는 0 또는 음수 값을 가질 수 없습니다.")
+    @ApiModelProperty(example = "2")
+    private Integer map_id;
+
+    public ParkingLotDto toParkingLot() {
+        return ParkingLotDto.builder()
+                .name(name.toUpperCase())
+                .location_id(location_id)
+                .map_id(map_id)
+                .build();
+    }
+
 }
