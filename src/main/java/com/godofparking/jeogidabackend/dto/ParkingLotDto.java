619007ff8c675ebf3802b1dbf1c9afc6563bb9ad
yujung7768903 <yu-jung31476@naver.com>
2022-08-23 16:11:24 +0900

    parking_lot 테이블 number 컬럼 name으로 수정

--- src/main/java/com/godofparking/jeogidabackend/dto/ParkingLotDto.java
+++ src/main/java/com/godofparking/jeogidabackend/dto/ParkingLotDto.java
@@ -1,6 +1,7 @@
 package com.godofparking.jeogidabackend.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -9,11 +10,19 @@ public class ParkingLotDto {
     private Integer id;
 
     @ApiModelProperty(example = "5")
-    private Integer number;
+    private String name;
 
     @ApiModelProperty(example = "1")
     private Integer location_id;
 
     @ApiModelProperty(example = "1")
     private Integer map_id;
+
+    @Builder
+    public ParkingLotDto(Integer id, String name, Integer location_id, Integer map_id) {
+        this.id = id;
+        this.name = name;
+        this.location_id = location_id;
+        this.map_id = map_id;
+    }
 }
---
+++ src/main/java/com/godofparking/jeogidabackend/dto/ParkingLotSaveRequestDto.java
@@ -0,0 +1,2 @@
+package com.godofparking.jeogidabackend.dto;public class ParkingLotSaveRequestDto {
+}
