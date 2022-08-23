b529629b4866dc17b1eced00c96f86a27b7990dc
yujung7768903 <yu-jung31476@naver.com>
2022-08-24 02:47:05 +0900

    user 수정 요청 형식 추가

--- src/main/java/com/godofparking/jeogidabackend/dto/UserUpdateRequestDto.java
+++ src/main/java/com/godofparking/jeogidabackend/dto/UserUpdateRequestDto.java
@@ -1,2 +1,27 @@
-package com.godofparking.jeogidabackend.dto;public class UserUpdateRequestDto {
+package com.godofparking.jeogidabackend.dto;
+
+import com.godofparking.jeogidabackend.validation.NullOrNotBlank;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+
+@Data
+public class UserUpdateRequestDto {
+
+    @NullOrNotBlank
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @ApiModelProperty(example = "yj06283l4@gmail.com")
+    private String email;
+
+    @NullOrNotBlank
+    @ApiModelProperty(example = "김유정")
+    private String nickname;
+
+    @NullOrNotBlank
+    @ApiModelProperty(example = "https://lh3.googleusercontent.com/a-/AFdZucpR_TN-i3qs-t0dQk-LFdHqSIUq_Wvv10iwcAIKSw=s96-c")
+    private String photo_url;
+
 }
